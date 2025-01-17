4fc5750de8fcf6b9831f7df3afe48ad3b82b4daa
trio223 <RinneTeemu@hotmail.com>
2025-01-17 22:33:54 +0200

    update hello.py

--- Moduuli2/T1_moduuli2.py
+++ Moduuli2/T1_moduuli2.py
@@ -5,4 +5,6 @@ print(f"moi {nimi}, mitä kuuluu")
 #huom minulla ei ole aiempaa lukio tai it taustaa
 #olen lähtenyt ihan alusta alkaen mutta vapaalla opiskelee tehokkaasti.
 #sähkö automaatio ja laboratorio taustaa on ja työkokemusta kummastakin.
+#työ kokemus sähkö töistä,lvi, työmaa, toimisto, laadunvalvonta, robotiikka, tuotanto
+#aattelin ottaa pääainevalinnaksi terveys teknologian hyvä pohja siihen
 #jos on vinkkejä lisä materiaaliin ihan alusta oppimiseen niin kerro ihmeessä olen kiinostunut.

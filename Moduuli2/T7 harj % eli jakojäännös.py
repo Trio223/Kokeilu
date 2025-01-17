925010a8e7f30091a094ba6c9530ca0e0ff57239
trio223 <RinneTeemu@hotmail.com>
2025-01-17 20:07:13 +0200

    update hello.py

---
+++ Moduuli2/T7 harj % eli jakojäännös.py
@@ -0,0 +1,26 @@
+#sen nimi on modulo :P
+#voi opetella suomeksi noi, mut sit ihmetyttää joskus jos joku puhuu modulosta, lol
+
+print(5 % 3)
+
+#ton output on kaksi, koska kolmonen menee kerran viitoseen, ja siitä jää kaksi yli
+#oikeenpuolimmainen tungetaan vasempaan niin monta kertaa kun vaan menee, ja se output on aina sitten se paljonko jää yli
+
+print(20 % 2)
+
+#kakkonen laitetaan 20 nii monta kertaa vaa ku menee. ja se menee 10 kertaa
+#jäljelle ei jää mitään, eli 0
+
+print(21 % 2)
+
+#no entäs toi
+#1?
+#tolla modulo kakkosella voi testata kans onko mikä tahansa luku parillinen!
+#aina jos vastaus on 1 nii se luku on pariton, jos 0 nii parillinen tietty
+#hyödyllinen
+
+x = 5
+print(x % 2)
+
+#vaihtelet vaan X, nii toi kertoo aina sit onko X parillinen tai pariton
+#jos se printtaa ulos 1, nii pariton, jos 0 nii parillinen
